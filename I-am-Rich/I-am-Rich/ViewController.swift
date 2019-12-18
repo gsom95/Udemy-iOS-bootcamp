db42adf8206bd9787039061407a77c4025ceaf14
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2019-12-18 17:22:21 +0300

    Initial commit

---
+++ I-am-Rich/I-am-Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I-am-Rich
+//
+//  Created by Igor Sapozhnikov on 27.11.2019.
+//  Copyright © 2019 Igor Sapozhnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
