2430ae528347f772a676382cfcd7000886238531
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2020-01-20 11:41:29 +0300

    Initial commit

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Igor Sapozhnikov on 18.01.2020.
+//  Copyright © 2020 Igor Sapozhnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+
+}
+
