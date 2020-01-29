7632c9fbb7c4421308a4389cdead49361d33a90d
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2020-01-29 17:32:44 +0300

    Add randomness to dices

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        leftDiceImageView.image = diceImages.randomElement()
+        rightDiceImageView.image = diceImages.randomElement()
+    }
 }
 
