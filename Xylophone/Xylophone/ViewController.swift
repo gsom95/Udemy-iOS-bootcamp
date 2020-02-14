1fb59326f17efb9c5bc968c9baf61643dff41fb6
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2020-02-14 15:41:24 +0300

    Update ViewController.swift
    Add playSound logic based on button tag.

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    let notes = ["C", "D", "E", "F", "G", "A", "B"]
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.2
+        playSound(notes[sender.tag-1])
+        
+        // Button is disabled during animation!
+        UIView.animate(withDuration: 0.1) {
+            sender.alpha = 1.0
+        }
+        
+    }
     
-
+    func playSound(_ note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
