312717d15cfc6cbb45bdb774b08665b9e746171e
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2020-02-17 15:42:55 +0300

    Add progress view
    Add progress view
    Fix timer countdown logic.

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -9,38 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+    
     let secondsInMinute = 1
-    var timerSeconds = 0
+    var totalTime = 0
+    var secondsPassed = 0
     
     var timer: Timer? = nil
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        if timer == nil {
-            switch sender.tag {
-            case 1:
-                timerSeconds = 5 * secondsInMinute
-            case 2:
-                timerSeconds = 7 * secondsInMinute
-            case 3:
-                timerSeconds = 12 * secondsInMinute
-            default:
-                print("Smth fucked up!")
-            }
-            
-            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-            timer?.tolerance = 0.3
-        } else {
-            print("Timer is running")
+        timer?.invalidate()
+        
+        totalTime = 0
+        secondsPassed = 0
+        
+        progressView.progress = 0
+        
+        switch sender.tag {
+        case 1:
+            totalTime = 5 * secondsInMinute
+            titleLabel.text = "So you want your eggs soft..."
+        case 2:
+            totalTime = 7 * secondsInMinute
+            titleLabel.text = "So you want your eggs medium"
+        case 3:
+            totalTime = 12 * secondsInMinute
+            titleLabel.text = "So you want your eggs hard!"
+        default:
+            print("Something is wrong")
+            return
         }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer!.tolerance = 0.3
+        
     }
     
     @objc func updateCounter() {
-        if timerSeconds > 0 {
-            print("\(timerSeconds) till the end")
-            timerSeconds -= 1
+        if secondsPassed < totalTime {
+            print("\(secondsPassed) has passed")
         } else {
             timer!.invalidate()
             timer = nil
-            print("That's all folks!")
+            titleLabel.text = "Done!"
         }
+        progressView.progress = Float(secondsPassed) / Float(totalTime)
+        secondsPassed += 1
     }
 }
