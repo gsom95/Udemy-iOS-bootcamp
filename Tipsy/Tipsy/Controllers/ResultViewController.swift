f8212edc979bbf21be7fc53a22d902f3d808bca1
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2020-03-03 13:59:06 +0300

    Connected Result UI to code

--- Tipsy/Tipsy/Controllers/ResultViewController.swift
+++ Tipsy/Tipsy/Controllers/ResultViewController.swift
@@ -9,22 +9,14 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var settingsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculateButtonPressed(_ sender: UIButton) {
     }
-    */
-
 }
