318af462ecd9c790259682d6fb616d0bc7f1ad33
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2020-03-13 11:32:28 +0300

    Add model manager methods

--- ByteCoin/ByteCoin/Controller/ViewController.swift
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var currencyLabel: UILabel!
+    @IBOutlet var bitcoinCostLabel: UILabel!
+    @IBOutlet var currencyPicker: UIPickerView!
+
+    let coinManager = CoinManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+
+        currencyLabel.text = coinManager.currencyArray[0]
     }
-
-
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let currentCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: currentCurrency)
+    }
+}
