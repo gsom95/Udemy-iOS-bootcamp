5f61303985b2cda483e5a7845324a39c7c6b9d5d
gsom95 <igorsapozhnikov9@gmail.com>
2020-06-29 16:42:56 +0300

    Create the view.
    Used ZStack for dividing background from the content, which is logo, 2 dice images and roll button.

--- Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var leftDiceNumber = Int.random(in: 1...6)
+    @State var rightDiceNumber = Int.random(in: 1...6)
+
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+
+                HStack {
+                    DiceView(diceNumber: leftDiceNumber)
+                    DiceView(diceNumber: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .background(Color(.red))
+            }
+            .padding(.vertical)
+        }
+    }
+}
+
+struct DiceView: View {
+    let diceNumber: Int
+
+    var body: some View {
+        Image("dice\(diceNumber)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
     }
 }
 
