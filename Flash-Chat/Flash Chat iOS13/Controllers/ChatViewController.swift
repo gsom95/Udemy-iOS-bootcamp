c83e7c46c7e1cd6ea3f222cae834141eeab07fda
gsom95 <igorsapozhnikov9@gmail.com>
2020-06-29 12:36:16 +0300

    UI update.
    Now logged in user can distinguish who sent which messages — user messages are with "avatar" circle on the right and other user messages are with "avatar" on the left.

--- Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -104,8 +104,22 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentMessage = messages[indexPath.row]
+
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label.text = messages[indexPath.row].body
+        cell.label.text = currentMessage.body
+
+        if currentMessage.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: Constants.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: Constants.BrandColors.purple)
+        } else {
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: Constants.BrandColors.purple)
+            cell.label.textColor = UIColor(named: Constants.BrandColors.lightPurple)
+        }
         
         return cell
     }
